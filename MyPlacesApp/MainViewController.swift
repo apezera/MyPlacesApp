50dececf1c5fe5ddf8235be858e79ff5ec52ebd8
Alexey Ezerskiy <apezera@icloud.com>
2023-01-10 20:32:22 +0300

    New Place View Controller (8 ypok)

--- MyPlacesApp/MainViewController.swift
+++ MyPlacesApp/MainViewController.swift
@@ -54,5 +54,7 @@ class MainViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func cancelAction(_ segue: UIStoryboardSegue) {}
 
 }
