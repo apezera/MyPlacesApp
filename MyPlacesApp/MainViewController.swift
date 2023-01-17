cd693ecd337d6b0bc815d71cfe46f1dea2c89d42
Alexey Ezerskiy <117728212+apezera@users.noreply.github.com>
2023-01-17 19:42:55 +0300

    Add files via upload

--- MyPlacesApp/MainViewController.swift
+++ MyPlacesApp/MainViewController.swift
@@ -11,7 +11,11 @@ import RealmSwift
 class MainViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var reversedSortingButton: UIBarButtonItem!
+    
     var places: Results<Place>!
+    var ascendingSorting = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -107,5 +111,26 @@ class MainViewController: UIViewController, UITableViewDataSource,UITableViewDel
         newPlaceVC.savePlace()
         tableView.reloadData()
     }
-
+    
+    @IBAction func sortSelection(_ sender: UISegmentedControl) {
+        
+        sorting()
+    }
+    
+    @IBAction func reversedSorting(_ sender: Any) {
+        
+        ascendingSorting.toggle()
+        if ascendingSorting { reversedSortingButton.image = #imageLiteral(resourceName: "AZ") }
+        else { reversedSortingButton.image = #imageLiteral(resourceName: "ZA") }
+        
+        sorting()
+    }
+    
+    private func sorting () {
+        
+        if segmentedControl.selectedSegmentIndex == 0 { places = places.sorted(byKeyPath: "date", ascending: ascendingSorting) }
+        else { places = places.sorted(byKeyPath: "name", ascending: ascendingSorting) }
+        
+        tableView.reloadData()
+    }
 }
